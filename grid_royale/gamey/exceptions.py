85407082223e27e74c5906f8797c77ca14704f18
Ram Rachum <ram@rachum.com>
2020-10-07 17:16:13 +0300

    -

--- grid_royale/gamey/exceptions.py
+++ grid_royale/gamey/exceptions.py
@@ -2,14 +2,14 @@ from __future__ import annotations
 
 from typing import Optional
 
-from .base import Action
+from .. import gamey
 
 
 class GameyException(Exception):
     pass
 
 class IllegalAction(GameyException):
-    def __init__(self, action: Optional[Action] = None) -> None:
+    def __init__(self, action: Optional[gamey.Action] = None) -> None:
         self.action = action or "the given action"
         GameyException.__init__(self, f"Can't play {action} in this state.")
 
